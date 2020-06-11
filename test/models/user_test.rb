61ddf9e00cc21ad4bb2cf0b0c86879a0577d496f
Uliks Sekiraqa <uliks@hotmail.co.uk>
2020-06-11 12:54:36 +0100

    added basic user tests to user_test.rb

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "invalid user" do
+    user = User.new
+    user.firstname = nil
+    user.lastname = nil
+    user.email = nil
+    user.username = nil
+    user.location = nil
+    user.password = nil
+    user.dob = nil
+    assert user.invalid?
+  end
+
+  test "valid user" do
+    user = User.new
+    user.firstname = 'Gilbert'
+    user.lastname = 'Humphrey'
+    user.email = 'gilbert@gmail.com'
+    user.username = 'gilbert'
+    user.password = 'password'
+    user.bio = 'Meeting new people!'
+    user.dob = '1999-05-28'
+    assert true
+  end
 end
+
